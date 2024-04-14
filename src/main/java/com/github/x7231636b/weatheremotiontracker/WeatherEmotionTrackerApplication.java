565003c35f779087d14f4b9051bf998686f01da9
Markus Hallmann <markus@hallmann.pw>
2024-04-14 17:08:33 +0200

    Added feign client for further implementations

--- src/main/java/com/github/x7231636b/weatheremotiontracker/WeatherEmotionTrackerApplication.java
+++ src/main/java/com/github/x7231636b/weatheremotiontracker/WeatherEmotionTrackerApplication.java
@@ -2,8 +2,10 @@ package com.github.x7231636b.weatheremotiontracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class WeatherEmotionTrackerApplication {
 
   public static void main(String[] args) {
